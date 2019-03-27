60fa30ae808ab47c0b6755eef12c9808d9ee40ad
Krystian Baranowski <krystian.qaz123@gmail.com>
2019-03-27 19:53:05 +0100

    register view

--- Common/views.py
+++ Common/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.views import View
+
+from Common.forms import UserRegisterForm
+
+
+class RegisterView(View):
+    template_name = 'register.html'
+    form_class = UserRegisterForm
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            password = form.cleaned_data['password1']
+            user.set_password(password)
+            user.save()
+            login(request, user)
+            return redirect('/')
+
+        return render(request, self.template_name, {'form': form})
