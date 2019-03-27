16ef23e02cc482ac95b1760a66d82e73040f661b
Krystian Baranowski <krystian.qaz123@gmail.com>
2019-03-27 15:11:30 +0100

    login/logout urls/ app installation

--- Project/settings.py
+++ Project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Common',
+    'Shop',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
