46ff6d584d41d5912f2615fe1e8068b49c6cd67f
Krystian Baranowski <krystian.qaz123@gmail.com>
2019-03-27 21:22:37 +0100

    models registered

--- Shop/admin.py
+++ Shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from Shop.models import Product, Order, ClientAdress
 
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(ClientAdress)
