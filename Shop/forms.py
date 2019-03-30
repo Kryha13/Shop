d675a0777bdc37bbe676c5bbcd9924fdd2381f45
Krystian Baranowski <krystian.qaz123@gmail.com>
2019-03-30 22:37:33 +0100

    cart add product form

--- Shop/forms.py
+++ Shop/forms.py
@@ -2,7 +2,7 @@
 from imagekit import ImageSpec, register
 from pilkit.processors import ResizeToFill
 
-from Shop.models import Product
+from Shop.models import Product, Order
 from django import forms
 
 
@@ -15,3 +15,20 @@ class AddProductForm(forms.ModelForm):
         model = Product
         fields = ['name', 'producer', 'description', 'price', 'image']
 
+
+class OrderForm(forms.ModelForm):
+
+    class Meta:
+        model = Order
+        fields = ['products']
+
+
+quantity_choices = [(i, str(i)) for i in range(1, 10)]
+
+
+class CartAddProductForm(forms.Form):
+    quantity = forms.TypedChoiceField(choices=quantity_choices,
+                                      coerce=int)
+    update = forms.BooleanField(required=False,
+                                initial=False,
+                                widget=forms.HiddenInput)
