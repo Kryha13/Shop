1527fd55bfb38eebb593a3481c46cc69f5bd5079
Krystian Baranowski <krystian.qaz123@gmail.com>
2019-03-30 22:37:16 +0100

    add/remove/update cart

--- Shop/views.py
+++ Shop/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy, reverse
 from django.views import generic, View
 from django.contrib import messages
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.views.decorators.http import require_POST
 from search_listview.list import SearchableListView
 from Shop.models import Product
-from Shop.forms import AddProductForm
+from Shop.forms import AddProductForm, OrderForm, CartAddProductForm
+from Shop.cart import Cart
 
 # Create your views here.
 
@@ -27,7 +29,8 @@ class ProductInfoView(View):
 
     def get(self, request, product_id):
         product = Product.objects.get(id=product_id)
-        return render(request, self.template_name, {'product': product})
+        cart_product_form = CartAddProductForm()
+        return render(request, self.template_name, {'product': product, 'cart_product_form': cart_product_form})
 
 
 class AddProductView(PermissionRequiredMixin, View):
@@ -76,6 +79,40 @@ class DeleteProductView(PermissionRequiredMixin, generic.DeleteView):
         return super(DeleteProductView, self).handle_no_permission()
 
 
+class OrderView(PermissionRequiredMixin, View):
+    permission_required = ('shop.add_order', 'shop.change_order', 'shop.delete_order')
+    template_name = 'order.html'
+    form_class = OrderForm
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
 
 
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product,
+                 quantity=cd['quantity'],
+                 update_quantity=cd['update'])
+    return redirect('Shop:cart_detail')
+
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('Shop:cart_detail')
+
+
+def cart_detail(request):
+    cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'],
+                                                                   'update': True})
+    return render(request, 'cart_detail.html', {'cart': cart})
 
