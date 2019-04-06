9d09ddc14afb64a878192dab267840dc45d89fe2
Krystian Baranowski <krystian.qaz123@gmail.com>
2019-04-06 12:15:44 +0200

    heroku attempt

--- Project/settings.py
+++ Project/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,6 +141,7 @@ STATICFILES_DIRS = [
     # os.path.join(BASE_DIR, "Common/static/media/CACHE/images/static/"),
 
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
